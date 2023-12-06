517d78aa9dd3c900a461fbdf14b3192699a0531a
K L <kinglaugood@gmail.com>
2023-12-06 12:20:43 -0500

    targeting from .NET 4.6 to 4.8

--- DBWeb/UserAccessModel.Designer.cs
+++ DBWeb/UserAccessModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Development\klau\webforms\DBWeb\DBWeb\UserAccessModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\development\klau\DemoPasswordKeeper\DBWeb\UserAccessModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
